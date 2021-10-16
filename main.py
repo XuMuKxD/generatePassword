a7f26e07c34d501046b19dbcec024cb4317b6cda
XuMuKxD <shabmish04@yandex.ru>
2021-10-16 14:03:50 +0300

    First commit

---
+++ main.py
@@ -0,0 +1,45 @@
+import random
+
+# Список символов
+ARRAY_SYMBOLS = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'a', 'b', 'c',
+                 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p',
+                 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C',
+                 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P',
+                 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', '@', '!', '#', '$',
+                 '%','=','&']
+
+# Получение количества символов в пароле
+CONST_COUNT_SYMBOLS = 6
+
+custom_count_symbols = int(input("Введите количество символов в пароле: "))
+
+if custom_count_symbols > 0:
+    count_symbols = custom_count_symbols
+else:
+    count_symbols = CONST_COUNT_SYMBOLS
+
+
+count_variant = len(ARRAY_SYMBOLS) ** count_symbols
+
+# Функция случайных символов
+def random_symbols():
+    return ARRAY_SYMBOLS[random.randint(0, len(ARRAY_SYMBOLS) - 1)]
+
+
+print(f'Количество доступных символов: {len(ARRAY_SYMBOLS)}')
+print(f'Количество возможных вариантов: {count_variant}')
+
+# Массив символов
+password_array = [i for i in range(0, count_symbols)]
+
+
+
+password = ''
+
+for i in password_array:
+    password = password + f'{random_symbols()}'
+
+print(f'Подобранный пароль: {password}')
+
+with open('password.txt', 'a') as password_str:
+    password_str.write('{}\n'.format(f'{password}'))
